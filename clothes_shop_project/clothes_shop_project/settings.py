ac8c7aad950858669be20862334bf6f34859c1fb
piotr.w.dabrowski2@gmail.com <Piotr Dbrowski>
2024-03-27 09:43:37 +0100

    base.html added

--- clothes_shop_project/clothes_shop_project/settings.py
+++ clothes_shop_project/clothes_shop_project/settings.py
@@ -109,6 +109,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'static/media'
--- clothes_shop_project/store_clothes/views.py
+++ clothes_shop_project/store_clothes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def store(request):
+    return render(request, 'store_clothes/store.html')
