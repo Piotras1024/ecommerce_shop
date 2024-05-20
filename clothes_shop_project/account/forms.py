e6a93b3a33140d53fcb237cb8033775647090f54
piotr.w.dabrowski2@gmail.com <Piotr Dbrowski>
2024-05-20 14:55:01 +0200

    accounts_done

--- clothes_shop_project/account/forms.py
+++ clothes_shop_project/account/forms.py
@@ -1,7 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 from django import forms
+
+from django.forms.widgets import PasswordInput, TextInput
+
+#Registration form
 
 
 class CreateUserForm(UserCreationForm):
@@ -29,6 +33,51 @@ class CreateUserForm(UserCreationForm):
             raise forms.ValidationError('Ten email jest nieprawidłowy')
 
         if len(email) >= 350:
+            raise forms.ValidationError('Ten email jest za długi')
+
+        return email
+
+
+# Login form
+
+class LoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=forms.TextInput())
+    password = forms.CharField(widget=forms.PasswordInput())
+
+# Update form
+
+
+class UpdateUserForm(forms.ModelForm):
+
+    password = None
+
+    class Meta:
+
+        model = User
+
+        fields = ['username', 'email']
+        exclude = ['password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateUserForm, self).__init__(*args, **kwargs)
+
+        # Mark email field as required
+
+        self.fields['email'].required = True
+
+    # Email validation
+
+    def clean_email(self):
+
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exclude(pk=self.instance.pk).exists():
+
+            raise forms.ValidationError('Ten email jest nieprawidłowy')
+
+        if len(email) >= 350:
+
             raise forms.ValidationError('Ten email jest za długi')
 
         return email
